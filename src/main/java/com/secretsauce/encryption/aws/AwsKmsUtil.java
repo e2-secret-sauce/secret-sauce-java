03d414126e308861c8a2545d4828794ebd40e478
John Killmer <killmer.john@gmail.com>
2019-02-16 23:40:54 -0500

    Decrypting with AWS KMS

--- src/main/java/com/secretsauce/encryption/aws/AwsKmsUtil.java
+++ src/main/java/com/secretsauce/encryption/aws/AwsKmsUtil.java
@@ -11,33 +11,48 @@ import java.util.Collections;
 import java.util.Map;
 
 public class AwsKmsUtil {
+
     private static Logger logger = LoggerFactory.getLogger(AwsKmsUtil.class);
+
+    public static final String KEY_ARN = "arn:aws:kms:us-east-1:775297465882:key/50837589-2e31-42ce-a327-d546767cda21";
 
     public String encrypt(String data){
 
-        String keyArn = "";
-
-        final AwsCrypto crypto = new AwsCrypto();
 
         // Set up the KmsMasterKeyProvider backed by the default credentials
-        final KmsMasterKeyProvider prov =
-                new KmsMasterKeyProvider()
+        final KmsMasterKeyProvider prov = KmsMasterKeyProvider
                         .builder()
                           .withCredentials(new ProfileCredentialsProvider("adfs"))
                           .withDefaultRegion(Regions.US_EAST_1.toString())
-                          .withKeysForEncryption(keyArn)
+                          .withKeysForEncryption(KEY_ARN)
                         .build();
 
-        // blogs.aws.amazon.com/security/post/Tx2LZ6WBJJANTNW/How-to-Protect-the-Integrity-of-Your-Encrypted-Data-by-Using-AWS-Key-Management
+        //blogs.aws.amazon.com/security/post/Tx2LZ6WBJJANTNW/How-to-Protect-the-Integrity-of-Your-Encrypted-Data-by-Using-AWS-Key-Management
         final Map<String, String> context = Collections.singletonMap("Example", "String");
 
-        final String ciphertext = crypto.encryptString(prov, data, context).getResult();
-        logger.info("plainText [{}] became cipherText [{}]", data, ciphertext);
+        final AwsCrypto crypto = new AwsCrypto();
+        final String cipherText = crypto.encryptString(prov, data, context).getResult();
+        logger.info("plainText [{}] became cipherText [{}]", data, cipherText);
 
-        return ciphertext;
+        return cipherText;
     }
 
     public String decrypt(String cipherText){
-        return "";
+        // Set up the KmsMasterKeyProvider backed by the default credentials
+        final KmsMasterKeyProvider prov = KmsMasterKeyProvider
+                .builder()
+                .withCredentials(new ProfileCredentialsProvider("adfs"))
+                .withDefaultRegion(Regions.US_EAST_1.toString())
+                .withKeysForEncryption(KEY_ARN)
+                .build();
+
+        //blogs.aws.amazon.com/security/post/Tx2LZ6WBJJANTNW/How-to-Protect-the-Integrity-of-Your-Encrypted-Data-by-Using-AWS-Key-Management
+        final Map<String, String> context = Collections.singletonMap("Example", "String");
+
+        final AwsCrypto crypto = new AwsCrypto();
+        String plainText = crypto.decryptString(prov, cipherText).getResult();
+        logger.info("cipherText [{}] became plainText[{}]", cipherText, plainText);
+        return plainText;
+
     }
 }
